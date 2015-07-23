aaf4c72f65af9aca670df56a7b2b09ce78d04bb5
Chris Coppens <chris.coppens@topic.nl>
2015-07-23 13:05:30 +0200

    Added copyright statement

--- topic-miami-monitor-lib.c
+++ topic-miami-monitor-lib.c
@@ -1,3 +1,30 @@
+/*
+ * topic-miami-monitor-lib.c
+ *
+ * Example and/or library for interpreting Topic Miami SOM monitor devices 
+ *
+ * (C) Copyright 2013,2015 Topic Embedded Products B.V. (http://www.topic.nl).
+ * All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA or see <http://www.gnu.org/licenses/>.
+ *
+ * You can contact Topic by electronic mail via info@topic.nl or via
+ * paper mail at the following address: Postbus 440, 5680 AK Best, The Netherlands.
+ */
+
 #include "topic-miami-monitor-lib.h"
 #include <dirent.h>
 #include <errno.h>
